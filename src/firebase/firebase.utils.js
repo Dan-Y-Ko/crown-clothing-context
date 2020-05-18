bb863bc9d80c0eb681b3511ceae571abc93d8dbf
Dan <danko56666@gmail.com>
2020-05-18 17:44:25 -0400

    first commit

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,16 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyADLRNyqILrrmhEcEd6zwyVU5oKaxDUrEg",
+  authDomain: "crwn-db-e4d30.firebaseapp.com",
+  databaseURL: "https://crwn-db-e4d30.firebaseio.com",
+  projectId: "crwn-db-e4d30",
+  storageBucket: "crwn-db-e4d30.appspot.com",
+  messagingSenderId: "698191699145",
+  appId: "1:698191699145:web:f1a918b2ae50f6c88ac118",
+  measurementId: "G-XD209Y6WMJ",
 };
 
 firebase.initializeApp(config);
@@ -29,10 +30,10 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
+        ...additionalData,
       });
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log("error creating user", error.message);
     }
   }
 
@@ -43,7 +44,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
